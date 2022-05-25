18161e1ed667f7422154b388358a8d7f04fd6f7a
Mariano <skynesplay@gmail.com>
2022-05-25 04:40:35 -0300

    Ejercicios Complementarios Level 3

--- EjerciciosComplementariosLevel3/Ejercicio5.java
+++ EjerciciosComplementariosLevel3/Ejercicio5.java
@@ -6,6 +6,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Se posee una Lista con objetos de clase Alumno con los atributos: apellido,
+ * nombre y fechaDeNacimiento (con tipos: String, String y LocalDate). Se desea
+ * generar un Map<String, Integer> donde la clave de Map será el apellido
+ * concatenado con el nombre (con separador de espacio en blanco) y el value la
+ * edad del alumno.
+ */
+
 public class Ejercicio5 {
     public static void main(String[] args) {
         List<Alumno> alumnos = List.of(
